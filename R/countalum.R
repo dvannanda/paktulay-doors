9c5a6f7053d14b2468d5519b22af7fe71570f709
Devi <dvannanda@gmail.com>
2021-10-12 13:57:54 +0800

    shiny app

---
+++ R/countalum.R
@@ -0,0 +1,69 @@
+#' Function to calculate number of aluminium bar needed 
+#' 
+#' e.g. countalum(x, barLength = 6000)
+#' 
+#' nested-function: onebar(x, barLength = 6000)
+
+
+# function to calculate how many cuts each bar fits
+onebar <- function(x, barLength = 6000) {
+  r = barLength
+  barCut = c()
+  needLeft = c()
+  
+  for (i in x) {
+    if (i < r) {
+      r = r - i
+      index = length(barCut) + 1
+      barCut[index] = i
+    } else {
+      index = length(needLeft) + 1
+      needLeft[index] = i
+    }
+  }
+  
+  output = list(barCut, needLeft)
+  
+  return(output)
+  
+}
+
+# function to calculate how many bars each material needs
+countalum <- function(x, barLength = 6000) {
+  
+  barNeeded = 0
+  needLeft = x
+  barcuts = c()
+  
+  while (length(needLeft > 0)) {
+    
+    barNeeded = barNeeded + 1
+    x = onebar(needLeft)
+    needLeft = x[[2]]
+    
+    index = length(barcuts) + 1
+    barcuts[[index]] <- x[[1]]
+  }
+  
+  remainder = list()
+  
+  for (i in barcuts) {
+    index = length(remainder) + 1
+    remainder[index] = 6000 - sum(i)
+  }
+  
+  output <-
+    tibble(
+      bar = 1:barNeeded, barcuts, remainder = remainder
+    ) %>% 
+    unnest(cols = c(barcuts, remainder)) %>% 
+    group_by(bar) %>% 
+    summarise(
+      bar_cuts = paste(barcuts, collapse = " + "),
+      remainder = min(remainder)
+    )
+  
+  return(output)
+  
+}
+
---
+++ countalum/app.R
@@ -0,0 +1,157 @@
+#' This is a shiny web application script
+#' Aim: To calculate the materials needed for an aluminium project build
+#' 
+#' Author: Devi Annanda
+#' Github: dvannanda
+
+library(shiny)
+library(tidyverse)
+library(reactable)
+
+library(shinyWidgets)
+
+# import data
+df <- read_csv(here::here("data/tidy-data.csv"))
+
+type <- df$tipe %>% unique()
+model <- df$model %>% unique()
+material <- df$materi %>% unique()
+
+source(here::here("R/countalum.R"))
+
+# UI -------------------------------------------------------------------------------------------------------------------
+ui <- fluidPage(
+    
+    theme = bslib::bs_theme(bootswatch = "flatly"),
+    
+    navbarPage(
+        
+        title = "Factory Aluminium Count",
+        
+        tabPanel(
+            "Home",
+            
+            sidebarLayout(
+                sidebarPanel(
+                    h2("Input"),
+                    tags$br(),
+                    
+                    pickerInput(
+                        inputId = "Id001",
+                        label = "Pilih Tipe",
+                        choices = type,
+                        selected = type,
+                        options = list(`actions-box` = TRUE),
+                        multiple = TRUE
+                    ),
+                    
+                    pickerInput(
+                        inputId = "Id002",
+                        label = "Pilih Model",
+                        choices = model,
+                        selected = model,
+                        options = list(`actions-box` = TRUE),
+                        multiple = TRUE
+                    ),
+                    
+                    pickerInput(
+                        inputId = "Id003",
+                        label = "Pilih Materi Aluminium",
+                        choices = material,
+                        selected = material,
+                        options = list(`actions-box` = TRUE),
+                        multiple = FALSE
+                    ),
+                    
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    tags$br(),
+                    
+                    downloadButton(
+                        outputId = "download",
+                        label = "Download CSV"
+                    )
+                    
+                ), # sidebarPanel
+                mainPanel(tabsetPanel(
+                    id = "tabset-output",
+                    tabPanel("# Bar Perlu",
+                             tags$br(),
+                             h5(textOutput("text1")),
+                             tags$br(),
+                             reactableOutput("output1")),
+                    tabPanel("Rincian",
+                             reactableOutput("output2"))
+                )) # tabsetPanel & mainPanel
+            ) # sidebarLayout
+        ), # tabPanel$home
+        tabPanel(
+            "About"
+        ) # tabPanel$about
+    ) # navbarPage
+) # fluidPage
+    
+
+# SERVER ---------------------------------------------------------------------------------------------------------------
+server <- function(input, output, session) {
+    
+    reacDataType <- reactive({
+        df %>% 
+            dplyr::filter(tipe %in% input$Id001)
+    })
+    
+    reacDataModel <- reactive({
+        reacDataType() %>% 
+            dplyr::filter(model %in% input$Id002)
+    })
+    
+    reacData <- reactive({
+        reacDataModel() %>% 
+            dplyr::filter(materi == input$Id003) %>% 
+            mutate(total = `perlu per unit` * unit) %>% 
+            group_by(ukuran) %>% 
+            summarise(total = sum(total))
+            
+    })
+
+    # change model input acc to type
+    observeEvent(input$Id001, {
+        updatePickerInput(
+            session = session,
+            inputId = "Id002",
+            choices = reacDataType()$model %>% unique(),
+            selected = reacDataType()$model %>% unique())
+    })
+    
+    # change 
+    observeEvent(input$Id002, {
+        updatePickerInput(
+            session = session,
+            inputId = "Id003",
+            choices = reacDataModel()$materi %>% unique() %>% sort(),
+            selected = reacDataModel()$materi %>% unique() %>% sort()
+        )
+    })
+    
+    reacNeed <- reactive({
+        rep(reacData()$ukuran, times = reacData()$total) %>% 
+            sort(decreasing = TRUE) %>% 
+            countalum()
+    })
+    
+    output$output1 <- renderReactable({
+        reacNeed() %>% 
+            reactable(pagination = FALSE)
+    })
+    
+    output$text1 <- renderText({
+        paste("No of bars needed for ", input$Id003, " = ", nrow(reacNeed()))
+    })
+    
+} # server
+
+
+# Run the application --------------------------------------------------------------------------------------------------
+shinyApp(ui = ui, server = server)
